f9ca4b391f3169d0717572b5dc50e6fe1ccad1b0
barakbenhur1 <barakbenhur@gmail.com>
2025-10-29 16:40:52 +0200

    Fix SwiftPM tools-version header (first line)

--- Package.swift
+++ Package.swift
@@ -2,26 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "ImageKit",
-    platforms: [
-        .iOS(.v15),
-        .tvOS(.v15),
-        .macCatalyst(.v15)
-    ],
-    products: [
-        .library(name: "ImageKit", targets: ["ImageKit"])
-    ],
-    targets: [
-        .target(
-            name: "ImageKit",
-            path: "Sources/ImageKit",
-            resources: [],
-            swiftSettings: [ .define("IMAGEKIT_SPM") ]
-        ),
-        .testTarget(
-            name: "ImageKitTests",
-            dependencies: ["ImageKit"],
-            path: "Tests/ImageKitTests"
-        )
-    ]
+  name: "ImageKit",
+  platforms: [
+    .iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8)
+  ],
+  products: [
+    .library(name: "ImageKit", targets: ["ImageKit"])
+  ],
+  targets: [
+    .target(name: "ImageKit"),
+    .testTarget(name: "ImageKitTests", dependencies: ["ImageKit"])
+  ]
 )
